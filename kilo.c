481e3b162edef589f4b2b602b3827da5ec0435d8
Gustavo Caso <gustavocaso@gmail.com>
2022-11-19 16:47:08 +0100

    get room for status bar

--- kilo.c
+++ kilo.c
@@ -409,10 +409,7 @@ void editorDrawRows(struct abuf *ab)
     }
 
     abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1)
-    {
-      abAppend(ab, "\r\n", 2);
-    }
+    abAppend(ab, "\r\n", 2);
   }
 }
 
@@ -569,6 +566,7 @@ void initEditor(int debug)
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
+  E.screenrows -= 1;
 }
 
 int main(int argc, char *argv[])
