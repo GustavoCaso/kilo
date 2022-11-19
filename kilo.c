1e3a203a8dce933c3282cef87eb02e85a93afdc4
Gustavo Caso <gustavocaso@gmail.com>
2022-11-19 13:41:36 +0100

    allow to move cursor pass contents of the screen

--- kilo.c
+++ kilo.c
@@ -43,6 +43,8 @@ typedef struct erow
 struct editorConfig
 {
   int cx, cy;
+  int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -288,12 +290,35 @@ void abFree(struct abuf *ab)
 
 /*** output ***/
 
+void editorScroll()
+{
+  if (E.cy < E.rowoff)
+  {
+    E.rowoff = E.cy;
+  }
+
+  if (E.cy >= E.rowoff + E.screenrows)
+  {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+  if (E.cx < E.coloff)
+  {
+    E.coloff = E.cx;
+  }
+
+  if (E.cx >= E.coloff + E.screencols)
+  {
+    E.coloff = E.cx - E.screencols + 1;
+  }
+}
+
 void editorDrawRows(struct abuf *ab)
 {
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    if (y >= E.numrows)
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows)
     {
       if (E.numrows == 0 && y == E.screenrows / 3)
       {
@@ -319,10 +344,12 @@ void editorDrawRows(struct abuf *ab)
     }
     else
     {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0)
+        len = 0;
       if (len < E.screencols)
         len = E.screencols;
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -335,6 +362,8 @@ void editorDrawRows(struct abuf *ab)
 
 void editorRefreshScreen()
 {
+  editorScroll();
+
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -343,7 +372,8 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+           (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
   abAppend(&ab, "\x1b[?25h", 6);
 
@@ -355,6 +385,8 @@ void editorRefreshScreen()
 
 void editorMoveCursor(int key)
 {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key)
   {
   case ARROW_LEFT:
@@ -364,7 +396,7 @@ void editorMoveCursor(int key)
     }
     break;
   case ARROW_RIGHT:
-    if (E.cx != E.screencols - 1)
+    if (row && E.cx < row->size)
     {
       E.cx++;
     }
@@ -376,11 +408,18 @@ void editorMoveCursor(int key)
     }
     break;
   case ARROW_DOWN:
-    if (E.cy != E.screenrows - 1)
+    if (E.cy != E.numrows)
     {
       E.cy++;
     }
     break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen)
+  {
+    E.cx = rowlen;
   }
 }
 
@@ -428,6 +467,8 @@ void initEditor()
 {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
